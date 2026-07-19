16aebcc26bc4a3bd324c646de04126fbff4796fe
Musa <m.aldemir70@outlook.com>
2026-07-19 22:06:51 +0300

    Varlıklar www klasörüne izole edildi, Tauri v2 güvenliği sağlandı

---
+++ www/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './',
+  base: './', 
+  build: {
+    outDir: 'dist',
+	emptyOutDir: true
+  },
+  server: {
+    watch: {
+      ignored: [
+        '**/C:/DumpStack.log.tmp',   // bu dosyayı ignore et
+        '**/node_modules/**',        // node_modules zaten ignore edilmeli
+        '**/dist/**'                 // build çıktısını izleme
+      ]
+    }
+  }
+});
